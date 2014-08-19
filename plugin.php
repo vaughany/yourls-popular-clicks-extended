36cf2b4bd4e2c4552384b006708d39629e44e3df
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-08-19 09:05:46 +0100

    Namespaced the functions, as I forgot.

--- plugin.php
+++ plugin.php
@@ -38,20 +38,20 @@ define ( "PCE_REL_DATE", '2014-07-16' );
 // Repository URL.
 define ( "PCE_REPO", 'https://github.com/vaughany/yourls-popular-clicks-extended' );
 
-yourls_add_action( 'plugins_loaded', 'popularclicksextended_add_page' );
+yourls_add_action( 'plugins_loaded', 'vaughany_popularclicksextended_add_page' );
 
-function popularclicksextended_add_page() {
-    yourls_register_plugin_page( 'popularclicksextended', 'Popular Clicks Extended', 'popularclicksextended_display_page' );
+function vaughany_popularclicksextended_add_page() {
+    yourls_register_plugin_page( 'popularclicksextended', 'Popular Clicks Extended', 'vaughany_popularclicksextended_display_page' );
 }
 
-function popularclicksextended_display_page() {
+function vaughany_popularclicksextended_display_page() {
 
     echo '<h2>Popular Clicks Extended</h2>' . "\n";
     echo '<p>This report shows the most popular clicks for the selected time periods as of ' . date( 'jS F Y, g:ia', time() ) . '.</p>' . "\n";
     echo '<p>Legend: <em>Position. Clicks | Short URL | Long URL</em></p>' . "\n";
 
     /**
-     * show_last_period(): queries the database for the number of clicks per link since n seconds ago,
+     * vaughany_show_last_period(): queries the database for the number of clicks per link since n seconds ago,
      *     e.g. 'time() - 300' to 'time()'
      *     e.g. '2014-07-15 14:52:27' to '2014-07-15 14:57:27'
      *
@@ -59,7 +59,7 @@ function popularclicksextended_display_page() {
      * $rows:       integer:    The number of rows to pull from the database (maximum), defaults to 10.
      * $desc:       string:     Describes the time period for the report.
      */
-    function show_last_period( $period, $rows, $desc ) {
+    function vaughany_show_last_period( $period, $rows, $desc ) {
 
         global $ydb;
 
@@ -84,7 +84,7 @@ function popularclicksextended_display_page() {
         }
 
         if ( $results = $ydb->get_results( $sql ) ) {
-            $out = render_results( $results );
+            $out = vaughany_render_results( $results );
         } else {
             $out = '<p>No results for the chosen time period.</p>' . "\n";
         }
@@ -98,7 +98,7 @@ function popularclicksextended_display_page() {
     }
 
     /**
-     * show_specific_period(): queries the database for the number of clicks per link per whole period,
+     * vaughany_show_specific_period(): queries the database for the number of clicks per link per whole period,
      *     e.g. 'today'
      *     e.g. '2014-07-15 00:00:00' to '2014-07-15 23:59:59'
      *
@@ -107,7 +107,7 @@ function popularclicksextended_display_page() {
      * $rows:   integer:    The number of rows to pull from the database (maximum), defaults to 10.
      * $desc:   string:     Describes the time period for the report.
      */
-    function show_specific_period( $period, $type, $rows, $desc ) {
+    function vaughany_show_specific_period( $period, $type, $rows, $desc ) {
 
         global $ydb;
 
@@ -157,7 +157,7 @@ function popularclicksextended_display_page() {
         }
 
         if ( $results = $ydb->get_results( $sql ) ) {
-            $out = render_results( $results );
+            $out = vaughany_render_results( $results );
         } else {
             $out = '<p>No results for the chosen time period.</p>' . "\n";
         }
@@ -173,7 +173,7 @@ function popularclicksextended_display_page() {
     /**
      * Often-used function to parse and format the results.
      */
-    function render_results( $results ) {
+    function vaughany_render_results( $results ) {
         $out = '<ol>';
         foreach ( $results as $result ) {
             $out .= '<li>';
@@ -188,11 +188,11 @@ function popularclicksextended_display_page() {
     }
 
     /**
-     * show_log() shows the n most recent lines from the log table.
+     * vaughany_show_log() shows the n most recent lines from the log table.
      *
      * $rows:   integer:    The number of rows to pull from the database (maximum), defaults to 10.
      */
-    function show_log( $rows ) {
+    function vaughany_show_log( $rows ) {
 
         global $ydb;
 
@@ -236,53 +236,53 @@ function popularclicksextended_display_page() {
     echo '<h2>Popular clicks for &quot;<em>period</em>&quot;</h2>' . "\n";
 
     /**
-     * show_specific_period() shows a specific hour, day, week, month or year. You can add more if you know what you're doing.
+     * vaughany_show_specific_period() shows a specific hour, day, week, month or year. You can add more if you know what you're doing.
      * TODO: Can we clean this up a bit, maybe a little refactoring?
      */
     // Specific hours.
-    show_specific_period( date( 'Y-m-d H', time() ), 'hour', null, 'this hour (' . date( 'jS F Y, ga', time() ) . ' to ' . date( 'ga', strtotime( '+ 1 hour' ) ) . ') (so far)' );
-    show_specific_period( date( 'Y-m-d H', strtotime( '- 1 hour' ) ), 'hour', null, 'the previous hour (' . date( 'jS F Y, ga', strtotime( '- 1 hour' ) ) . ' to ' . date( 'ga', time() ) . ')' );
+    vaughany_show_specific_period( date( 'Y-m-d H', time() ), 'hour', null, 'this hour (' . date( 'jS F Y, ga', time() ) . ' to ' . date( 'ga', strtotime( '+ 1 hour' ) ) . ') (so far)' );
+    vaughany_show_specific_period( date( 'Y-m-d H', strtotime( '- 1 hour' ) ), 'hour', null, 'the previous hour (' . date( 'jS F Y, ga', strtotime( '- 1 hour' ) ) . ' to ' . date( 'ga', time() ) . ')' );
     // Specific days.
-    show_specific_period( date( 'Y-m-d', time() ), 'day', null, 'today (' . date( 'jS F Y', time() ) . ') (so far)' );
-    show_specific_period( date( 'Y-m-d', strtotime( '- 1 day' ) ), 'day', null, 'yesterday (' . date( 'jS F Y', strtotime( '- 1 day' ) ) . ')' );
+    vaughany_show_specific_period( date( 'Y-m-d', time() ), 'day', null, 'today (' . date( 'jS F Y', time() ) . ') (so far)' );
+    vaughany_show_specific_period( date( 'Y-m-d', strtotime( '- 1 day' ) ), 'day', null, 'yesterday (' . date( 'jS F Y', strtotime( '- 1 day' ) ) . ')' );
     // Specific weeks:
-    show_specific_period( date( 'Y-m-d', strtotime( 'last monday' ) ), 'week', null, 'this week (beginning ' . date( 'jS F Y', strtotime( 'last monday' ) ) . ') (so far)' );
-    show_specific_period( date( 'Y-m-d', strtotime( 'last monday - 7 days' ) ), 'week', null, 'last week  (beginning ' . date( 'jS F Y', strtotime( 'last monday - 7 days' ) ) . ')' );
+    vaughany_show_specific_period( date( 'Y-m-d', strtotime( 'last monday' ) ), 'week', null, 'this week (beginning ' . date( 'jS F Y', strtotime( 'last monday' ) ) . ') (so far)' );
+    vaughany_show_specific_period( date( 'Y-m-d', strtotime( 'last monday - 7 days' ) ), 'week', null, 'last week  (beginning ' . date( 'jS F Y', strtotime( 'last monday - 7 days' ) ) . ')' );
     // Specific months:
-    show_specific_period( date( 'Y-m', time() ), 'month', null, 'this month (' . date( 'F Y', time() ) . ') (so far)' );
-    show_specific_period( date( 'Y-m', strtotime( '- 1 month' ) ), 'month', null, 'last month (' . date( 'F Y', strtotime( '- 1 month' ) ) . ')' );
+    vaughany_show_specific_period( date( 'Y-m', time() ), 'month', null, 'this month (' . date( 'F Y', time() ) . ') (so far)' );
+    vaughany_show_specific_period( date( 'Y-m', strtotime( '- 1 month' ) ), 'month', null, 'last month (' . date( 'F Y', strtotime( '- 1 month' ) ) . ')' );
     // Specific years:
-    //show_specific_period( date( 'Y', time() ), 'year', null, 'this year (' . date( 'Y', time() ) . ') (so far)');
-    //show_specific_period( date( 'Y', strtotime( '- 1 year' ) ), 'year', null, 'last year (' . date('Y', strtotime( '- 1 year' ) ) . ')' );
+    vaughany_show_specific_period( date( 'Y', time() ), 'year', null, 'this year (' . date( 'Y', time() ) . ') (so far)');
+    vaughany_show_specific_period( date( 'Y', strtotime( '- 1 year' ) ), 'year', null, 'last year (' . date('Y', strtotime( '- 1 year' ) ) . ')' );
 
     echo "<hr>\n";
     echo '<h2>Popular clicks for the last &quot;<em>period</em>&quot;</h2>' . "\n";
 
     /**
-     * show_last_period() shows all clicks from n seconds ago until now. Note that 24 hours here is not the same as 'yesterday', above.
+     * vaughany_show_last_period() shows all clicks from n seconds ago until now. Note that 24 hours here is not the same as 'yesterday', above.
      */
-    show_last_period( 60 * 5,                   null, '5 minutes');
-    show_last_period( 60 * 30,                  null, '30 minutes');
-    show_last_period( 60 * 60,                  null, 'hour');
-    show_last_period( 60 * 60 * 6,              null, '6 hours');
-    show_last_period( 60 * 60 * 12,             null, '12 hours');
-    show_last_period( 60 * 60 * 24,             null, '24 hours');
-    show_last_period( 60 * 60 * 24 * 2,         null, '2 days');
-    show_last_period( 60 * 60 * 24 * 7,         null, 'week');
-    show_last_period( 60 * 60 * 24 * 14,        null, '2 weeks');
-    show_last_period( 60 * 60 * 24 * 30,        null, 'month');
-    show_last_period( 60 * 60 * 24 * 60,        null, '2 months');
-    show_last_period( 60 * 60 * 24 * 90,        null, '3 months');
-    show_last_period( 60 * 60 * 24 * 180,       null, '6 months');
-    //show_last_period( 60 * 60 * 24 * 365,       null, 'year');
-    //show_last_period( 60 * 60 * 24 * 365 * 2,   null, '2 years');
+    vaughany_show_last_period( 60 * 5,                  null, '5 minutes');
+    vaughany_show_last_period( 60 * 30,                 null, '30 minutes');
+    vaughany_show_last_period( 60 * 60,                 null, 'hour');
+    vaughany_show_last_period( 60 * 60 * 6,             null, '6 hours');
+    vaughany_show_last_period( 60 * 60 * 12,            null, '12 hours');
+    vaughany_show_last_period( 60 * 60 * 24,            null, '24 hours');
+    vaughany_show_last_period( 60 * 60 * 24 * 2,        null, '2 days');
+    vaughany_show_last_period( 60 * 60 * 24 * 7,        null, 'week');
+    vaughany_show_last_period( 60 * 60 * 24 * 14,       null, '2 weeks');
+    vaughany_show_last_period( 60 * 60 * 24 * 30,       null, 'month');
+    vaughany_show_last_period( 60 * 60 * 24 * 60,       null, '2 months');
+    vaughany_show_last_period( 60 * 60 * 24 * 90,       null, '3 months');
+    vaughany_show_last_period( 60 * 60 * 24 * 180,      null, '6 months');
+    vaughany_show_last_period( 60 * 60 * 24 * 365,      null, 'year');
+    vaughany_show_last_period( 60 * 60 * 24 * 365 * 2,  null, '2 years');
     // ...and the catch-all:
-    //show_last_period( time(),                   null, 'billion years');
+    vaughany_show_last_period( time(),                  null, 'billion years');
 
     echo "<hr>\n";
     echo '<h2>Recently used short links</h2>' . "\n";
 
-    show_log( 10 );
+    vaughany_show_log( 10 );
 
     echo "<hr>\n";
 
